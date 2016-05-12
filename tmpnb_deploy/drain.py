b4120bc9d181509a36bc6e9676d5ddd90b01b2b4
Peter Parente <pparent@us.ibm.com>
2016-05-11 21:40:23 -0400

    Fix typo

--- tmpnb_deploy/drain.py
+++ tmpnb_deploy/drain.py
@@ -11,7 +11,7 @@ def main():
     resp = requests.delete('http://127.0.0.1:10000/api/pool')
     resp.raise_for_status()
     n = resp.json()['drained']
-    print('Drained %d idle containers from the pull.' % n)
+    print('Drained %d idle containers from the pool.' % n)
     print('The pool will refill after the --cull_period.')
 
 if __name__ == '__main__':
